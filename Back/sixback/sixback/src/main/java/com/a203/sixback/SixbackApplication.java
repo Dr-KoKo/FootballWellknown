bd2f7ebbdfaea10b767021128681879455709b7f
박철민 <pcm0720@gmail.com>
2022-10-17 14:24:11 +0900

    feat : Start project
    feat : Start project

---
+++ Back/sixback/sixback/src/main/java/com/a203/sixback/SixbackApplication.java
@@ -0,0 +1,13 @@
+package com.a203.sixback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixbackApplication.class, args);
+	}
+
+}
