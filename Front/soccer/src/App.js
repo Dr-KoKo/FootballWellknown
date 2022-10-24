b54dcc3db298cc530a97883ff8f1f88aebc3f609
Hong Seok In <hrpt8@naver.com>
2022-10-24 17:14:53 +0900

    feat : Add Sample Code

--- Front/soccer/src/App.js
+++ Front/soccer/src/App.js
@@ -1,7 +1,20 @@
 import logo from "./logo.svg";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/teams/ranks");
+      setData(response);
+      console.logo(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  getData();
   console.log(1);
   return (
     <div className="App">
