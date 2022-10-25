8c31fb3590ef1b9ec3b828e3775cec87eec4023b
Hong Seok In <hrpt8@naver.com>
2022-10-25 17:53:26 +0900

    feat : TeamDetail Frame

--- Front/soccer/src/pages/Team/TeamDetail.js
+++ Front/soccer/src/pages/Team/TeamDetail.js
@@ -4,22 +4,30 @@ import { useParams } from "react-router";
 
 const TeamDetail = () => {
   const {id} = useParams();
-  const [datas, setDatas] = useState(null);
+  const [datas, setDatas] = useState({teamInfo : "", finMatch : [], yetMatch:[]});
   useEffect(() => {
     axios.get(`http://localhost:8080/api/v1/teams/${id}/details`).then((response) => {
+        console.log(response.data.result);
       setDatas(response.data.result);
     });
   }, []);
   return (<div>
-    <div>{datas.teamInfo.name}</div>
+    <div> 클럽명 : {datas.teamInfo.name}</div>
     <div><img src={datas.teamInfo.image} alt=""></img></div>
-    <div>{datas.teamInfo.win}</div>
-    <div>{datas.teamInfo.draw}</div>
-    <div>{datas.teamInfo.lose}</div>
-    <div>{datas.teamInfo.goals}</div>
-    <div>{datas.teamInfo.loseGoals}</div>
-    <div>{datas.teamInfo.rank}</div>
-    <div>{datas.teamInfo.pts}</div>
+    <div>승 : {datas.teamInfo.win}</div>
+    <div>무 : {datas.teamInfo.draw}</div>
+    <div>패 : {datas.teamInfo.lose}</div>
+    <div>득점 :{datas.teamInfo.goals}</div>
+    <div>실점 : {datas.teamInfo.loseGoals}</div>
+    <div>순위 : {datas.teamInfo.rank}</div>
+    <div>평점 : {datas.teamInfo.pts}</div>
+    <div>
+        {datas.finMatch.map((data) =>(
+            <div key={data.matchId}>
+            <div>{data.matchId}</div>
+            </div>
+        ))}
+    </div>
   </div>);
 };
 
