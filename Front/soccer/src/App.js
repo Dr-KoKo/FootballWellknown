f3dd66fc331d00c5c5942821c7c44dbdfb500f62
Hong Seok In <hrpt8@naver.com>
2022-10-25 16:20:23 +0900

    feat : TeamInfo

--- Front/soccer/src/App.js
+++ Front/soccer/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Home from './Home';
-import About from './About';
-import Layout from './components/Layout';
-import { Route, Routes } from 'react-router';
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Layout from "./components/Layout";
+import { Route, Routes } from "react-router";
+import TeamInfo from "./pages/Team/TeamInfo";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/teaminfo" element={<TeamInfo />} />
       </Routes>
     </Layout>
   );
