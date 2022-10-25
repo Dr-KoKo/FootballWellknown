c2ec66ab712f7da6d177017378b86b6536d052a3
Hong Seok In <hrpt8@naver.com>
2022-10-25 17:13:03 +0900

    feat : gradle

--- Back/sixback/src/main/java/com/a203/sixback/SixbackApplication.java
+++ Back/sixback/src/main/java/com/a203/sixback/SixbackApplication.java
@@ -10,12 +10,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 @EnableScheduling
 public class SixbackApplication {
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
-			+"classpath:application-key.properties";
+//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//			+ "classpath:application.yml";
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SixbackApplication.class)
-				.properties(APPLICATION_LOCATIONS)
 				.run(args);
 	}
 
