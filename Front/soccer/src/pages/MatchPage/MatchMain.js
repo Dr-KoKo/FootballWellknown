6669284e85f8324816b1cd001332698d2f9747ff
kotaehee <koxogml@naver.com>
2022-10-25 15:58:00 +0900

    feat: ADD materialUI modules

---
+++ Front/soccer/src/pages/MatchPage/MatchMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MatchMain = ({month}) => {
+    console.log(month);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MatchMain;
