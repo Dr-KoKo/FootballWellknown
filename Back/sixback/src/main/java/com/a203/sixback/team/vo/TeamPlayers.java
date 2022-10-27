2ce0f5b78ae3f62d7945270d137a9a9300e8d75e
Hong Seok In <hrpt8@naver.com>
2022-10-27 18:00:06 +0900

    feat : Team Service

--- Back/sixback/src/main/java/com/a203/sixback/team/vo/TeamPlayers.java
+++ Back/sixback/src/main/java/com/a203/sixback/team/vo/TeamPlayers.java
@@ -12,7 +12,10 @@ import java.util.List;
 @Getter
 @Setter
 public class TeamPlayers {
-    private List<PlayerVO> players;
+    private List<PlayerVO> fws;
+    private List<PlayerVO> dfs;
+    private List<PlayerVO> mfs;
+    private List<PlayerVO> gks;
     private String coachName;
     private String coachImage;
     private int coachAge;
