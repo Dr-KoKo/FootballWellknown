c6902bd6f1abc55ca302c71e2b8144f3d86fc16d
kotaehee <koxogml@naver.com>
2022-10-28 16:01:50 +0900

    Feature/fe/match page

--- Front/soccer/src/components/Match/MatchPredict.js
+++ Front/soccer/src/components/Match/MatchPredict.js
@@ -1,10 +1,9 @@
-import axios from "axios";
-import React, { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router";
-import Grid from "@mui/material/Grid";
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import Grid from '@mui/material/Grid';
 
 const MatchPredict = () => {
-  console.log(124364);
   const params = useParams();
   const matchId = params.matchId;
   const [predicts, setPredicts] = useState([]);
@@ -12,52 +11,49 @@ const MatchPredict = () => {
   const [draw, setDraw] = useState(0);
   const [awayWin, setAwayWin] = useState(0);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8080/api/v1/matches/predict/match/all/${matchId}`)
-      .then((res) => {
-        let data = res.data.result;
-        setPredicts(res.data.result);
-        let homeWinCnt = 0;
-        let drawCnt = 0;
-        let awayCnt = 0;
-        data.map((predict) => {
-          if (predict.whereWin === "HOME") homeWinCnt++;
-          if (predict.whereWin === "DRAW") drawCnt++;
-          if (predict.whereWin === "AWAY") awayCnt++;
-        });
-        setHomeWin(homeWinCnt);
-        setDraw(drawCnt);
-        setAwayWin(awayCnt);
+  useEffect(()=>{
+    axios.get(`http://localhost:8080/api/v1/matches/predict/match/all/${matchId}`)
+    .then(res => {
+      let data = res.data.result;
+      setPredicts(res.data.result);
+      let homeWinCnt = 0;
+      let drawCnt = 0;
+      let awayCnt = 0;
+      data.map((predict) => {
+        if(predict.whereWin === "HOME") homeWinCnt++;
+        if(predict.whereWin === "DRAW") drawCnt++;
+        if(predict.whereWin === "AWAY") awayCnt++;
       });
+      setHomeWin(homeWinCnt);
+      setDraw(drawCnt);
+      setAwayWin(awayCnt);
+    });
   }, []);
 
   return (
     <Fragment>
-      <Fragment>총 {predicts.length}명이 참여했습니다.</Fragment>
-      <br />
+      <Fragment>
+        총 {predicts.length}명이 참여했습니다.
+      </Fragment>
+      <br/>
       <Grid container>
         <Grid item xs={4}>
           <p>본머스</p>
-          <p>
-            {predicts.length !== 0 ? (homeWin / predicts.length) * 100 : 0}%
-          </p>
+          <p>{predicts.length !== 0 ? ((homeWin / predicts.length) * 100) : 0}%</p>
         </Grid>
         <Grid item xs={4}>
           <p>무승부</p>
-          <p>{predicts.length !== 0 ? (draw / predicts.length) * 100 : 0}%</p>
+          <p>{predicts.length !== 0 ? ((draw / predicts.length) * 100) : 0}%</p>
         </Grid>
         <Grid item xs={4}>
           <p>본머스</p>
-          <p>
-            {predicts.length !== 0 ? (awayWin / predicts.length) * 100 : 0}%
-          </p>
+          <p>{predicts.length !== 0 ? ((awayWin / predicts.length) * 100) : 0}%</p>
         </Grid>
       </Grid>
       <Fragment>
         <ul>
-          {predicts.map((predict) => (
-            <li>
+          {predicts.map((predict, index) => (
+            <li key={index}>
               {predict.userNickname}
               {predict.whereWin === "HOME" && "홈팀 승리"}
               {predict.whereWin === "DRAW" && "무승부"}
