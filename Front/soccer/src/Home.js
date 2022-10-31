348d6f5aa922fbead9b220f53e6b365f9914ec3e
kotaehee <koxogml@naver.com>
2022-10-31 09:26:37 +0900

    Feature/fe/match page

--- Front/soccer/src/Home.js
+++ Front/soccer/src/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>홈</h1>
       <p>이곳은 홈이에요. 가장 먼저 보여지는 페이지죠.</p>
+      <Link to={'/match'}>매치</Link>
+      <br/>
+      <Link to={'/teaminfo'}>팀인포</Link>
+      <br/>
     </div>
   );
 };
--- Front/soccer/src/components/Match/PlayerEvaluate.js
+++ Front/soccer/src/components/Match/PlayerEvaluate.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
 
 const PlayerEvaluate = () => {
-    return (
-        <div>
-            PlayerEvaluate
-        </div>
-    );
+  const params = useParams();
+  const matchId = params.matchId;
+  
+
+  useEffect(()=>{
+    axios.get(`http://localhost:8080/api/v1/matches/statistics/${matchId}`)
+    .then(res => {
+      let data = res.data.result;
+      console.log(data);
+    })
+  },[]);
+  return (
+    <div>
+      PlayerEvaluate
+    </div>
+  );
 };
 
 export default PlayerEvaluate;
