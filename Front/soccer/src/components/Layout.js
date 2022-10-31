9ac955e13fc632501b842dbc3aea93bbc1be6fa4
Hong Seok In <hrpt8@naver.com>
2022-10-31 14:07:56 +0900

    feat : Layout

--- Front/soccer/src/components/Layout.js
+++ Front/soccer/src/components/Layout.js
@@ -1,8 +1,19 @@
 import React, { Fragment } from 'react';
-
+import Logo from './assets/logo.png';
+import { NavLink } from 'react-router-dom';
+import './Layout.css';
 const Layout = (props) => {
   return (
     <Fragment>
+        <nav className='navbar'>
+          <img id='logo' src={Logo} alt=''></img>
+          <ul  id='ul'>
+            <li  id='li'><NavLink className='navbar_li' to="/"><div>HOME</div></NavLink></li>
+            <li  id='li'><NavLink className='navbar_li' to="teaminfo"><div>TEAM</div></NavLink></li>
+            <li  id='li'><NavLink className='navbar_li' to="match"><div>MATCH</div></NavLink></li>
+            <li  id='li'><NavLink className='navbar_li' to="/"><div>COMMUNITY</div></NavLink></li>            
+          </ul>
+        </nav>
         <main>{props.children}</main>
     </Fragment>
   );
