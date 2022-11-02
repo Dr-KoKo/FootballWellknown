299e1c87e042ed89ed40641c666d9c58a321ad2e
양택훈 <yth0909@nate.com>
2022-11-02 16:59:34 +0900

    feat: develop match info component

--- Front/soccer/src/Home.js
+++ Front/soccer/src/Home.js
@@ -4,55 +4,48 @@ import './Home.css';
 import TeamInfo from 'pages/Team/TeamInfo';
 import axios from 'axios';
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useDispatch } from 'react-redux';
+import Carousel from 'react-material-ui-carousel'
+import { IconButton } from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 
-const day = ['(일)','(월)','(화)','(수)','(목)','(금)','(토)']
+import { useDispatch } from 'react-redux';
+import Item from 'Item';
+import HorizonLine from 'components/HorizonLine';
+
+
 
 const Home = () => {
   const [teamToggle, setTeamToggle] = useState(true);
   const [matchInfo, setMatchInfo] = useState([]);
-  const [Loading, setLoading] =useState(true);
-  const [round, setRound] = useState("7");
+  const [round, setRound] = useState(9);
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch();
+  const [carousel, setCarousel] = useState([]);
+  
+
+  const item = [
+    {
+        name: "Random Name #1",
+        description: "Probably the most random thing you have ever seen!"
+    },
+    {
+        name: "Random Name #2",
+        description: "Hello World!"
+    }
+];
 
 
-  const moveToDetail = async (matchId) => {
-    await axios.get(`http://localhost:8080/api/v1/matches/match/${matchId}`)
-    .then((res) => {
-      console.log(res.data.result);
-      const payload = res.data.result.matchVO;
-      let matchStatus = "";
-      if(res.data.result.matchStatus === "FIN"){
-        matchStatus = "경기종료";
-      }
-      else if(res.data.result.matchStatus === "DELAY"){
-        matchStatus = "지연";
-      }
-      else{
-        matchStatus = "경기전";
-      }
-      dispatch({
-        type: "SET_MATCH",
-        payload,
-        matchStatus,
-      })
-    });
-    window.location.href = `/match/${matchId}/MatchPredict`;
-  }
+
 
   useEffect(() => {
     axios.get(`http://localhost:8080/api/v1/matches/round?round=${round}`).then((response) => {
       setMatchInfo(response.data.result);
       setLoading(false);
+      const len = response.data.result.length;
+      setCarousel([[response.data.result.slice(0,len/2)], [response.data.result.slice(len/2,len)]]);
     });
-  }, []);
+  }, [round]);
 
   const teamButtonClicked = () =>{
     setTeamToggle(true);
@@ -60,67 +53,45 @@ const Home = () => {
   const playerButtonClicked = () =>{
     setTeamToggle(false);
   }
+  const leftButtonClicked = () =>{
+    if(round - 1 >= 1){
+      setRound(round-1);
+    } 
+  }
+  const rightButtonClicked = () =>{
+    if(round + 1 <= 38){
+      setRound(round+1);
+    } 
+  }
+  
+  
   return (
     <div id='homeContainer'>
       <div id='home-userRank'>
         <h1>여기에 유저 포인트 랭킹 컴포넌트를 넣을거에요~</h1>
       </div>
-      <div>
+     
       <div id='home-matchPlan'>
-        <h1>{`현재 라운드: ${round}`}</h1>
-        {(matchInfo.length !== 0 ? 
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-            <TableCell>날짜</TableCell>
-              <TableCell align="center">시간</TableCell>
-              <TableCell align="center">경기</TableCell>
-              <TableCell align="right">장소</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {matchInfo.map((data) => (
-            <TableRow
-            hover
-            key={data.matchVO.matchId}
-            onClick={() => moveToDetail(data.matchVO.matchId)}
-            sx={{ '&:last-child TableCell, &:last-child th': { border: 0 } }}
-          >
-            {/* 날짜 */}
-            <TableCell> 
-              {data.matchVO.date.split('T')[0].split('-')[1]}.
-              {data.matchVO.date.split('T')[0].split('-')[2]}
-              {day[new Date(data.matchVO.date).getDay()]}
-            </TableCell>
-            {/* 시간 */}
-            <TableCell>
-              {data.matchVO.date.split('T')[1]}
-            </TableCell>
-            {/* 경기 */}
-            <TableCell align="center">
-              <span style={{marginRight: 10}}>{data.matchVO.home}</span>
-              <img src={data.matchVO.homeImage} alt="home" width={'5%'}/>
-              <span style={{marginLeft: 10, marginRight: 10}}>
-                {data.matchStatus === "FIN"
-                ?
-                data.matchVO.homeScore + " vs " + data.matchVO.awayScore
-                :
-                " vs "
-                }
-              </span>
-              <img src={data.matchVO.awayImage} alt="away" width={'5%'}/> 
-              <span style={{marginLeft: 10}}>
-                {data.matchVO.away}
-              </span> 
-            </TableCell>
-            {/* 장소 */}
-            <TableCell align="right">{data.matchVO.stadium}</TableCell>
-            </TableRow>
-          ))}
-          </TableBody>
-        </Table>
-        : null)}
+        <div className='home-round'>
+          <IconButton onClick={leftButtonClicked}>
+            <ArrowLeftIcon fontSize='large'/>
+          </IconButton>
+          <h1>{`현재 라운드: ${round}`}</h1>
+          <IconButton onClick={rightButtonClicked}>
+            <ArrowRightIcon fontSize='large'/>
+          </IconButton>
+        </div>
+        
+        {carousel.length !== 0 && carousel[0][0].length !== 0 ? 
+        <Carousel 
+          sx={{ minWidth:"80%"}}
+        >
+            {
+                carousel.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel> : <h2>아직 예정된 경기가 없습니다.</h2>}
       </div>
+      
       <div id='home-rank'>
         <div style={{marginBottom: "0.5rem"}}>
           <button className='home-categoryButton' id={teamToggle ? "curr-home-categoryButton" : null} onClick={teamButtonClicked}>팀 순위</button>
@@ -129,9 +100,9 @@ const Home = () => {
         </div>
           {teamToggle ? <TeamInfo count={10}></TeamInfo> : <div>안녕</div>}
       </div>
-      </div>
     </div>
   );
 };
+
 
 export default Home;
