31b1e42a6bd4764ba9087037719504ac1ef5aaf7
Denny-Ko <donghar@naver.com>
2022-11-02 17:47:44 +0900

    fix: Get user point

--- Back/sixback/src/main/java/com/a203/sixback/user/UserService.java
+++ Back/sixback/src/main/java/com/a203/sixback/user/UserService.java
@@ -1,13 +1,11 @@
 package com.a203.sixback.user;
 
 import com.a203.sixback.auth.UserPrincipal;
-import com.a203.sixback.db.entity.Board;
-import com.a203.sixback.db.entity.CommentMongo;
-import com.a203.sixback.db.entity.Predict;
-import com.a203.sixback.db.entity.User;
+import com.a203.sixback.db.entity.*;
 //import com.a203.sixback.db.mongo.CommentRepoMongoDB;
 import com.a203.sixback.db.mongo.CommentRepoMongoDB;
 import com.a203.sixback.db.repo.BoardRepo;
+import com.a203.sixback.db.repo.PointLogRepo;
 import com.a203.sixback.db.repo.PredictRepo;
 import com.a203.sixback.user.res.*;
 import lombok.RequiredArgsConstructor;
@@ -21,11 +19,13 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
     private final BoardRepo boardRepo;
+    private final PointLogRepo pointLogRepo;
     private final CommentRepoMongoDB commentRepoMongoDB;
     private final PredictRepo predictRepo;
 
-    public UserService(BoardRepo boardRepo, CommentRepoMongoDB commentRepoMongoDB, PredictRepo predictRepo) {
+    public UserService(BoardRepo boardRepo, PointLogRepo pointLogRepo, CommentRepoMongoDB commentRepoMongoDB, PredictRepo predictRepo) {
         this.boardRepo = boardRepo;
+        this.pointLogRepo = pointLogRepo;
         this.commentRepoMongoDB = commentRepoMongoDB;
         this.predictRepo = predictRepo;
     }
@@ -71,6 +71,8 @@ public class UserService {
 
         User user = ((UserPrincipal)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser();
 
-        return ResGetUserPointDTO.of(200,"성공", user.getPoint());
+        List<ResGetUserPointDTO.GetPointDTO> pointLogList = pointLogRepo.findAllByUser(user).stream().map(ResGetUserPointDTO.GetPointDTO::new).collect(Collectors.toList());
+
+        return ResGetUserPointDTO.of(200,"성공", pointLogList);
     }
 }
