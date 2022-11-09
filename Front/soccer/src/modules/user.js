5198989a61cf7637513e74b9695b106a97881f4b
korHoonie <97661931+korHoonie@users.noreply.github.com>
2022-11-09 16:28:43 +0900

    feat: renew access token

--- Front/soccer/src/modules/user.js
+++ Front/soccer/src/modules/user.js
@@ -23,6 +23,8 @@ export const initState = {
 export default function (state = initState, action) {
   switch (action.type) {
     case SET_TOKEN:
+      console.log("Tlqkf")
+      console.log(action.payload)
       return {
         ...state,
         token: action.payload,
--- Front/soccer/src/services/axios.js
+++ Front/soccer/src/services/axios.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { store } from "..";
+import { getToken } from "./userServices";
+import { SET_TOKEN} from "modules/types";
+import { useDispatch } from "react-redux";
+
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 
 /**
  * 인증 필요없는 axios
  */
 
 export const request = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: process.env.REACT_APP_LOCAL_SERVER_URL,
+  // baseURL: process.env.REACT_APP_SERVER_URL,
   withCredentials: true,
 });
 
@@ -26,7 +32,8 @@ request.interceptors.response.use(
  */
 
 export const axiosAuth = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: process.env.REACT_APP_LOCAL_SERVER_URL,
+  // baseURL: process.env.REACT_APP_SERVER_URL,
   timeout: 10000,
   withCredentials: true,
   
@@ -47,14 +54,33 @@ axiosAuth.interceptors.request.use(
 );
 
 axiosAuth.interceptors.response.use(
-  (response) => {
+  function (response) {
     return response;
   },
-
-  // 에러 처리
-  (error) => {
-    console.log("axiosAuth interceptors response 에러!!")
+  // 에러처리
+  async function(error) {
+    const result = error.config;
     console.log(error)
-    return Promise.reject(error);
+    console.log(error.response.status)
+    if(error.response.status === 401){
+      const res = await getToken();
+      const accessToken = res.data.body.token;
+      store.dispatch({ type: SET_TOKEN, payload:  accessToken  })
+      location.reload();
+      
+      
+
+    }
+    return Promise.reject(error)
+
+
   }
+
+  // // 에러 처리
+  // (error) => {
+  //   console.log(error.config)
+  //   console.log("axiosAuth interceptors response 에러!!")
+  //   console.log(error)
+  //   return Promise.reject(error);
+  // }
 );
