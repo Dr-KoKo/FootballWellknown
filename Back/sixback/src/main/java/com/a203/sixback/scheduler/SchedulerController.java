660220652b7b9077ce039fc3622046ac98134aad
Denny-Ko <donghar@naver.com>
2022-11-21 09:33:04 +0900

    fix: Redis for ranking

--- Back/sixback/src/main/java/com/a203/sixback/scheduler/SchedulerController.java
+++ Back/sixback/src/main/java/com/a203/sixback/scheduler/SchedulerController.java
@@ -59,7 +59,7 @@ public class SchedulerController {
     }
 
     @Async
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 38 9 * * *")
     public void dailyRankingRefreshSchedule() throws Exception {
         if (isSchedulerServer){
             log.info("Initiate Delete Daily Ranking");
@@ -69,7 +69,7 @@ public class SchedulerController {
     }
 
     @Async
-    @Scheduled(cron = "0 0 0 * * 0")
+    @Scheduled(cron = "0 38 9 * * *")
     public void weeklyRankingRefreshSchedule() throws Exception {
         if (isSchedulerServer){
             log.info("Initiate Delete Weekly Ranking");
